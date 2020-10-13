1441c180a396023339cf90accbe20aa7c800747b
Vhuynh25 <43254910+Vhuynh25@users.noreply.github.com>
2020-10-12 19:46:37 -0700

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -6,9 +6,9 @@
 #include <algorithm>
 #include <iomanip>
 
-const std::vector<std::string> keywords = {"int", "float", "return", "function", "while", "if", "fi", "put"};
-const std::vector<char> seps = { '[', ']', '(', ')', '{', '}', ';', ':'  ,'$', ',' };
-const std::vector<char> ops = { '=', '!', '<', '>', '-', '+', '*', '/'};
+const std::vector<std::string> keywords = {"int", "float", "return", "function", "while", "if", "fi", "put" , "Boolean" , "real" , "get" , "true" , "false"};
+const std::vector<std::string> seps = { "[", "]", "(", ")", "{", "}", ";", ":"  , "$$" , ",", "$"};
+const std::vector<std::string> ops = { "=", "!", "<", ">", "-", "+", "*", "/" , "<=", ">=", "!=", "-=", "+="};
 
 class record {
 private:
@@ -21,50 +21,67 @@ public:
 	void setToken(std::string s) { this->token = s; }
 };
 
+bool FSM(std::string &state, char input, std::string &lexeme) {
+	std::string c;
+	c.push_back(input);
+	if (state == "start") {
+		if (isalpha(input)) { state = "identifier"; }
+		else if (isdigit(input)) { state = "int"; }
+		else if (input == '.') { state = "dot"; }
+		else if (std::find(ops.begin(), ops.end(), c) != ops.end()) { state = "operator"; }
+		else if (std::find(seps.begin(), seps.end(), c) != seps.end()) { state = "separator"; }
+		else if (input == EOF) {
+			state = "fileend";
+			return true;
+		}
+	}
+
+	else if (state == "identifier" && !isalnum(input) && input != '_') {
+			return true;
+		}
+	else if (state == "int") {
+		if (input == '.') { state = "dot"; }
+		else if (!isdigit(input)) {
+			return true;
+		}
+	}
+	else if (state == "dot") {
+		if (isdigit(input)) {
+			state = "real";
+		}
+		else {
+			state = "int";
+			return true;
+		}
+	}
+	else if (state == "real" && !isdigit(input)) {
+		return true;
+	}
+	else if (state == "operator" && std::find(ops.begin(), ops.end(), lexeme + c) == ops.end()) {
+		return true;
+	}
+	else if (state == "separator" && std::find(seps.begin(), seps.end(), lexeme + c) == seps.end()) {
+		return true;
+	}
+
+	return false;
+}
+
 record callLexer(std::ifstream& source) {
 	std::string state = "start", lexeme = "";
 	int done = 0;
 	char c;
 	while (done != 1) {
 		c = source.get();
-		// \std::cout << c;
-		if (state == "start") {
-			if (isalpha(c)) { state = "identifier"; }
-			else if (isdigit(c)) { state = "int"; }
-			else if (std::find(ops.begin(), ops.end(), c) != ops.end()) { state = "operator"; }
-			else if (std::find(seps.begin(), seps.end(), c) != seps.end()) { state = "separator";}
-			else if (c == EOF) {
-				state = "fileend";
-				done = 1;
-			}
-		}
-		else if (state != "comments" && lexeme == "/*") { 
-			state = "comments"; 
-			lexeme = "";
+
+		if (FSM(state, c, lexeme) == true) { 
+			done = 1; 
+			source.unget();
 		}
 
-		else if (state == "identifier" && !isalnum(c)) { 
-			done = 1; 
-			source.unget();
-		}
-		else if (state == "int") {
-			if (c == '.') { state = "real"; }
-			else if (!isdigit(c)) {
-				done = 1;
-				source.unget();
-			}
-		}
-		else if (state == "real" && !isdigit(c)) {
-			done = 1;
-			source.unget();
-		}
-		else if (state == "operator" && std::find(ops.begin(), ops.end(), c) == ops.end()) { 
-			done = 1; 
-			source.unget();
-		}
-		else if (state == "separator") { 
-			done = 1; 
-			source.unget();
+		if (state != "comments" && lexeme == "/*") {
+			state = "comments";
+			lexeme = "";
 		}
 		else if (state == "comments" && c == '*' && (c = source.get()) == '/') {
 			state = "start";
